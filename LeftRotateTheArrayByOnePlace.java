112f72cbcf31330036b1db27138190e13066fb17
Piyush Rajak <112371639+PiyushR22@users.noreply.github.com>
2024-05-08 10:08:37 +0530

    Create LeftRotateTheArrayByOnePlace.java

---
+++ LeftRotateTheArrayByOnePlace.java
@@ -0,0 +1,22 @@
+public class LeftRotateTheArrayByOnePlace{
+
+    static void left_rotate(int arr[], int n) {
+        int temp = arr[0];
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[n - 1] = temp;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int arr[] = { 1, 2, 3, 4, 5 };
+
+        left_rotate(arr, n);
+
+    }
+}
